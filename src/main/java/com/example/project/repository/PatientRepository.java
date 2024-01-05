7bba5833df1314b8f516abbedc6f44b2f19caeae
FatiId04 <fatimaalzahraidrissi03@gmail.com>
2024-01-05 13:09:25 +0100

    Revert "initial commit"
    This reverts commit c16cad82be2d066e1f51cacfd284b85caceaa85e.

--- src/main/java/com/example/project/repository/PatientRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.project.repository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import com.example.project.model.Patient;
-
-public interface PatientRepository  extends MongoRepository<Patient, String>  {
-
-}
